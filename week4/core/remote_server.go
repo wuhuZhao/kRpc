0a89aeb13d2eb3cb65274895496bdd695742b33d
wuhuZhao <hkzhao123@gmail.com>
2022-11-09 19:30:25 +0800

    fix: server for loop

--- week4/core/remote_server.go
+++ week4/core/remote_server.go
@@ -22,15 +22,18 @@ func (k *krpcServer) Init() error {
 
 // 真正的transport调用，只需要传入封装好的中间件以及call，最后实现一个 encode-> (middleware -> call) - > decode 的模型
 func (k *krpcServer) Transport(conn net.Conn, ep Endpoint, ch chan error) {
-	req, resp := &Message{}, &Message{RpcInfo: &RpcInfo{}}
-	if er := k.co.Decode(conn, req); er != nil {
-		ch <- er
-	}
-	if err := ep(context.Background(), req, resp); err != nil {
-		ch <- err
-	}
-	if er := k.co.Encode(conn, resp); er != nil {
-		ch <- er
+	// 多次调用同一个conn的消息
+	for {
+		req, resp := &Message{}, &Message{RpcInfo: &RpcInfo{}}
+		if er := k.co.Decode(conn, req); er != nil {
+			ch <- er
+		}
+		if err := ep(context.Background(), req, resp); err != nil {
+			ch <- err
+		}
+		if er := k.co.Encode(conn, resp); er != nil {
+			ch <- er
+		}
 	}
 }
 
